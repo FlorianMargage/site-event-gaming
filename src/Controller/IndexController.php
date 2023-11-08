512711a7b3592805bc3c4b79ca364a43e42417fa
Florian Margage <florian.margage@hotmail.com>
2023-11-08 11:23:25 +0100

    create develop branch

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+// src/Controller/IndexController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IndexController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', []);
+    }
+}
